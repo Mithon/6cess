d1e75a9b400a6733457431431676fa63d314ce40
Hunter Caron <huntercaron@me.com>
2016-03-31 00:57:22 -0400

    lots of updates
    name page nearly done
    put all js dependancies in one file
    and a bunch more stuff

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,9 @@ var panels = [document.getElementById("panel0"), document.getElementById("panel1
 var currentTab = tabs[0];
 var mainOpen = false;
 var hoodData;
+var activeParam = 'population';
+var paramScales;
+var compareHoods = [];
 
 explore.addEventListener("click", toggleMain);
 topIcon.addEventListener("click", toggleMain);
@@ -16,13 +19,29 @@ tabs[0].addEventListener("click", function() { makeTransition(this); });
 tabs[1].addEventListener("click", function() { makeTransition(this); });
 tabs[2].addEventListener("click", function() { makeTransition(this); });
 
-Array.min = function( array ){
-    return Math.min.apply( Math, array );
+var terms = {
+    "population": "Population",
+    "areaCode": "Area Code",
+    "avgRating": "Average Yelp Score",
+    "avgRCount": "Average Yelp Rating Count",
+    "walkscore": "Walkscore",
+    "homePrices": "Average Home Price",
+    "assaults": "Assaults",
+    "arsons": "Arsons",
+    "robberies": "Robberies"
 };
 
-Array.max = function( array ){
-    return Math.max.apply( Math, array );
-};
+WebFontConfig = {
+    google: { families: [ 'Roboto+Mono:300:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
 
 function makeTransition(tab) {
 
@@ -101,7 +120,6 @@ var yAxis = d3.svg.axis()
     .scale(z)
     .orient("left");
 
-
 var svg = d3.select(".word-plot").append("svg")
     //.attr("class", "word-svg")
     .attr("width", width + margin.left + margin.right)
@@ -109,8 +127,42 @@ var svg = d3.select(".word-plot").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var wordGwidths = window.innerWidth*0.6;
+
+var hRatings = d3.select(".top-words").append("svg")
+    .attr('width', (wordGwidths+100))
+    .attr('height', 300)
+    .append("g");
+
+var lRatings = d3.select(".bottom-words").append("svg")
+    .attr('width', (wordGwidths/2+100))
+    .attr('height', 300)
+    .append("g");
+
+var hOccur = d3.select(".most-words").append("svg")
+    .attr('width', (wordGwidths/2+100))
+    .attr('height', 300)
+    .append("g");
+
 d3.json("../data/allWords.json", function(error, data) {
     if (error) throw error;
+
+    var wordsRatingsArr = (data.slice().sort(compareRatings));
+    var wordsHRatings = wordsRatingsArr.slice(0, 6);
+    var wordsLRatings = wordsRatingsArr.slice(wordsRatingsArr.length-6, wordsRatingsArr.length).reverse();
+    var wordsHOccur = data.slice().sort(compareNumbers).slice(0,6);
+
+    var wordHRatingsScale = d3.scale.linear()
+        .range([0, wordGwidths])
+        .domain([d3.min(wordsHRatings, function(d) { return d.rating; }), d3.max(wordsHRatings, function(d) { return d.rating; })]);
+
+    var wordLRatingsScale = d3.scale.linear()
+        .range([0, (wordGwidths/2)])
+        .domain([d3.min(wordsLRatings, function(d) { return d.rating; }), d3.max(wordsLRatings, function(d) { return d.rating; })]);
+
+    var wordHOccurScale = d3.scale.linear()
+        .range([0, wordGwidths/2])
+        .domain([d3.min(wordsHOccur, function(d) { return d.number; }), d3.max(wordsHOccur, function(d) { return d.number; })]);
 
     x.domain(data.map(function(d) { return d.word.charAt(0); }));
     y.domain([d3.min(data, function(d) { return d.number }), d3.max(data, function(d) { return d.number })]);
@@ -119,7 +171,14 @@ d3.json("../data/allWords.json", function(error, data) {
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(xAxis)
+        .append("text")
+        .attr("x", width-(width/5))
+        .attr("y", -5)
+        .attr("dx", ".2em")
+        .style("text-anchor", "start")
+        .text("x: alphabetical order // radius: occurances");
+
 
     svg.append("g")
         .attr("class", "y axis")
@@ -145,13 +204,98 @@ d3.json("../data/allWords.json", function(error, data) {
         .attr("rx", function(d) { return y(d.number); })
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide);
+
+
+    var hRatingsBar = hRatings.selectAll(".word-bars")
+        .data(wordsHRatings)
+        .enter().append('g')
+        .attr("transform", function(d, i) { return "translate(0," + i * 38 + ")"; });
+
+
+    hRatingsBar.append('rect')
+        .attr('class', "word-bars")
+        .attr('width', function(d) { return wordHRatingsScale(d.rating)+100; })
+        .attr('height', 30)
+        .attr("dy", ".35em")
+
+    hRatingsBar.append("text")
+        .attr('class', "word-bar-text")
+        .attr("x", 8)
+        .attr("y", 15)
+        .attr("dy", ".35em")
+        .text(function(d) { return `${d.word} - ${d.rating}`; });
+
+    var lRatingsBar = lRatings.selectAll(".word-bars")
+        .data(wordsLRatings)
+        .enter().append('g')
+        .attr("transform", function(d, i) { return "translate(0," + i * 38 + ")"; });
+
+    lRatingsBar.append('rect')
+        .attr('class', "word-bars")
+        .attr('width', function(d) { return wordLRatingsScale(d.rating)+100; })
+        .attr('height', 30)
+        .attr("dy", ".35em")
+
+    lRatingsBar.append("text")
+        .attr('class', "word-bar-text")
+        .attr("x", 8)
+        .attr("y", 15)
+        .attr("dy", ".35em")
+        .text(function(d) { return `${d.word} - ${d.rating}`; });
+
+    var hOccurBar = hOccur.selectAll(".word-bars")
+        .data(wordsHOccur)
+        .enter().append('g')
+        .attr("transform", function(d, i) { return "translate(0," + i * 38 + ")"; });
+
+    hOccurBar.append('rect')
+        .attr('class', "word-bars")
+        .attr('width', function(d) { return wordHOccurScale(d.number)+100; })
+        .attr('height', 30)
+        .attr("dy", ".35em");
+
+    hOccurBar.append("text")
+        .attr('class', "word-bar-text")
+        .attr("x", 8)
+        .attr("y", 15)
+        .attr("dy", ".35em")
+        .text(function(d) { return `${d.word} - ${d.number}`; });
+
 });
 
+function compareWords(param) {
+    function compare(a,b) {
+      if (a[param] < b[param])
+        return -1;
+      else if (a[param] > b[param])
+        return 1;
+      else
+        return 0;
+    }
+}
+
+function compareRatings(a,b) {
+  if (a.rating > b.rating)
+    return -1;
+  else if (a.rating < b.rating)
+    return 1;
+  else
+    return 0;
+}
+
+function compareNumbers(a,b) {
+  if (a.number > b.number)
+    return -1;
+  else if (a.number < b.number)
+    return 1;
+  else
+    return 0;
+}
 
 // MAP STUFF
 
 
-tip = d3.tip()
+mapTip = d3.tip()
     .attr('class', 'd3-tip')
     .html(function(d) {
         return `<h4>${d.properties.AREA_NAME.split('(')[0]}</h4>
@@ -165,6 +309,9 @@ var mapHeight = 900;
 var map = d3.select(".map-diagram").append("svg")
     .attr('class', 'toronto-map');
 
+//var selectedHoodOne = d3.select(".selection-one").append("svg")
+    //.attr('class', 'hood-map hood-one');
+
 d3.json("../data/hoods.json", function(error, toronto) {
     if (error) throw error;
 
@@ -176,7 +323,7 @@ d3.json("../data/hoods.json", function(error, toronto) {
     var path = d3.geo.path()
         .projection(projection);
 
-    map.call(tip);
+    map.call(mapTip);
 
     var hoods = topojson.feature(toronto, toronto.objects.hoods);
     var b = d3.geo.bounds(hoods);
@@ -186,15 +333,25 @@ d3.json("../data/hoods.json", function(error, toronto) {
         (b[1][1] - b[0][1]) / (mapHeight-0)
     )]);
 
-
     map.selectAll(".hood")
     .data(hoods.features)
     .enter().append("path")
     .attr('transform', `translate(-${80}) rotate (-34, ${mapWidth/2}, ${mapHeight/2})`)
     .attr("class", function(d) { return "hood " + "n"+d.properties.AREA_CODE; })
     .attr("d", path)
-    .on('mouseover', tip.show)
-    .on('mouseout', tip.hide);
+    .on('mouseover', mapTip.show)
+    .on('mouseout', mapTip.hide)
+    .on("click", function(d) {
+        var hoodElementHTML = document.querySelector(`.n${d.properties.AREA_CODE}`);
+
+        addCompareHood(d.properties.AREA_CODE);
+        if (classie.has(hoodElementHTML, 'active-hood')) {
+            classie.remove(hoodElementHTML, 'active-hood');
+        } else {
+            classie.add(hoodElementHTML, 'active-hood');
+        }
+
+    });
 
     /*
     map.append("path")
@@ -215,42 +372,130 @@ d3.json("../data/hoods.json", function(error, toronto) {
         .attr("d", path)
         .attr('transform', `translate(-${80}) rotate (-34, ${mapWidth/2}, ${mapHeight/2})`);
 
+
+    /*
+    var selectedHoodPath = document.querySelector(".n004");
+
+    var hoodOne = document.querySelector(".hood-one");
+
+    var cln = selectedHoodPath.cloneNode(true);
+    cln.setAttribute('transform',`translate(-420, -420)`);
+
+    hoodOne.appendChild(cln);
+    */
+
+
     d3.json("../data/hoodDataInfo.min.json", function(error, json) {
             if (error) return console.warn(error);
             hoodData = json;
+
+            paramScales = {
+                "population": createScale("population"),
+                "areaCode": createScale("areaCode"),
+                "avgRating": createScale("avgRating"),
+                "avgRCount": createScale("avgRCount"),
+                "walkscore": createScale("walkscore"),
+                "homePrices": createScale("homePrices"),
+                "assaults": createScale("assaults"),
+                "arsons": createScale("arsons"),
+                "robberies": createScale("robberies")
+            };
+
             colourMap();
     });
 });
 
-function colourMap() {
-    var scales = [
-        d3.scale.linear()
-            .domain([25, 50, 75, 100])
-            .range(["red", "orange", "yellow", "white"])
-    ];
-    var scaleMe = d3.scale.linear()
-        .domain([25, 50, 75, 100])
-        .range(["red", "orange", "yellow", "white"]);
+function colourMap(param) {
+    param = param || 'population';
+
     for (var i in hoodData) {
         var id = ('000' + hoodData[i].areaCode).substr(-3);
         var hood = document.querySelector((`.n${id}`));
-
-        hood.style.fill = createScale('walkscore', ['Teal','salmon'])(hoodData[i].walkscore);
-        //hood.style.opacity = hoodData[i].walkscore/100;
+        hood.style.fill = paramScales[param](hoodData[i][param]);
     }
 }
 
-function createScale(dataParam, colors) {
+function createScale(param, colors) {
+    colors = colors || ['#19232C','#00FFEA'];
     var totalVars = [];
     for (var i of hoodData) {
-        totalVars.push(i[dataParam]);
+        totalVars.push(i[param]);
     }
     return d3.scale.linear()
-        .domain([Array.min(totalVars), Array.max(totalVars)])
+        .domain([d3.min(totalVars), d3.max(totalVars)])
         .range([colors[0], colors[1]]);
 }
 
+function addCompareHood(areaCode) {
+    var d = hoodData[parseInt(areaCode)-1];
+    d.AREA_CODE = areaCode;
+    if (compareHoods.length > 1) {
+        classie.remove(document.querySelector((`.n${compareHoods[0].AREA_CODE}`)), 'active-hood');
 
+        compareHoods.shift();
+        compareHoods.push(d);
+    } else {
+        compareHoods.push(d);
+    }
+    console.log(compareHoods);
+
+    if (compareHoods[0].hasOwnProperty("name"))
+        document.querySelector(".hood-title-one").innerHTML = compareHoods[0].name;
+
+
+
+    var hoodOne = document.querySelector(".selection-one-info");
+    var hoodTwo = document.querySelector(".selection-two-info");
+
+    hoodOne.innerHTML = " ";
+    hoodTwo.innerHTML = " ";
+
+    for (var param in terms)  {
+        if (compareHoods[0].hasOwnProperty("name")) {
+            if (param == 'avgRating' || param == 'avgRCount') {
+                hoodOne.innerHTML = hoodOne.innerHTML + `<p>${terms[param]}: ${compareHoods[0][param].toFixed(2)}</p>`;
+            } else {
+                hoodOne.innerHTML = hoodOne.innerHTML + `<p>${terms[param]}: ${compareHoods[0][param]}</p>`;
+            }
+            console.log("SHIT");
+        }
+    }
+
+    if (compareHoods[1].hasOwnProperty("name"))
+        document.querySelector(".hood-title-two").innerHTML = compareHoods[1].name;
+
+    for (var param in terms)  {
+        if (compareHoods[1].hasOwnProperty("name")) {
+            if (param == 'avgRating' || param == 'avgRCount') {
+                hoodTwo.innerHTML = hoodTwo.innerHTML + `<p>${terms[param]}: ${compareHoods[0][param].toFixed(2)}</p>`;
+            } else {
+                hoodTwo.innerHTML = hoodTwo.innerHTML + `<p>${terms[param]}: ${compareHoods[0][param]}</p>`;
+            }
+            console.log("SHIT2");
+        }
+    }
+
+}
+
+function changeTip(param) {
+
+    mapTip.html(function(d) {
+        if (param == 'avgRating' || param == 'avgRCount') {
+            var dataText = (hoodData[parseInt(d.properties.AREA_CODE)-1][param]).toFixed(2);
+        } else {
+            var dataText = (hoodData[parseInt(d.properties.AREA_CODE)-1][param]);
+        }
+        return `<h4>${hoodData[parseInt(d.properties.AREA_CODE)-1].name}</h4>
+            <span>${terms[param]}:${dataText}
+            </span>`;
+    })
+
+}
+
+function changeMap(param) {
+    colourMap(param);
+    changeTip(param);
+}
 
 
 
